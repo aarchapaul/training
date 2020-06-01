5c78e417a624877a4018e0d3ea764ea5e3f1931a
AarchaP <40428184+aarchapaul@users.noreply.github.com>
2020-06-01 21:05:57 +0530

    Add files via upload

---
+++ week1.js
@@ -0,0 +1,6 @@
+alert("hello world");
+var name=prompt("Your name");
+console.log(name);
+document.write("<h1>Hi</h1>");
+document.getElementById("test").innerHTML = "Time to learn Javascript";
+console.log("helloo")

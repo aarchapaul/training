d8cffe54d2f188a5bb1de0c26db314795d3d1c60
aarchapaul <aarchapaul194@gmail.com>
2020-06-01 21:05:13 +0530

    version 0.1

--- Untitled-1.js
+++
@@ -1,6 +0,0 @@
-alert("hello world");
-var name=prompt("Your name");
-console.log(name);
-document.write("<h1>Hi</h1>");
-document.getElementById("test").innerHTML = "Time to learn Javascript";
-console.log("helloo")

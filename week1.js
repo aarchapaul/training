ec76a0e161f581acd2fa14b80af099b2e4b9ee32
aarchapaul <aarchapaul194@gmail.com>
2020-06-02 10:52:51 +0530

    commit

--- week1.js
+++ week1.js
@@ -2,5 +2,8 @@ alert("hello world");
 var name=prompt("Your name");
 console.log(name);
 document.write("<h1>Hi</h1>");
-document.getElementById("test").innerHTML = "Time to learn Javascript";
+
 console.log("helloo")
+function myFunction(){
+  document.getElementById("test").innerHTML = "Time to learn Javascript";
+}

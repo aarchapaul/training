537de516908b39d52650e611607b83fb5dfa6b61
aarchapaul <aarchapaul194@gmail.com>
2020-06-04 08:47:55 +0530

    using this keyword

---
+++ week-2.js
@@ -0,0 +1,19 @@
+function upDate(previewPic){
+  var x = previewPic.document.getElementsByClassName("preview").alt;
+  //console.log();
+  document.getElementsById("image").innerHTML = x;
+  document.getElementsByClassName("preview").style.backgroundImage = "url('previewPic.src')";
+  //previewPic.document.
+}
+ 
+function unDo(){
+
+      /* In this function you should 
+     1) Update the url for the background image of the div with the id = "image" 
+     back to the orginal-image.  You can use the css code to see what that original URL was
+     
+     2) Change the text  of the div with the id = "image" 
+     back to the original text.  You can use the html code to see what that original text was
+     */
+     
+}

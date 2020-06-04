86568b662b97aad022c0b70b60bc94993fe73b1a
aarchapaul <aarchapaul194@gmail.com>
2020-06-05 00:10:44 +0530

    image displayed

--- week-2.js
+++ week-2.js
@@ -1,19 +1,14 @@
 function upDate(previewPic){
-  var x = previewPic.document.getElementsByClassName("preview").alt;
-  //console.log();
-  document.getElementsById("image").innerHTML = x;
-  document.getElementsByClassName("preview").style.backgroundImage = "url('previewPic.src')";
-  //previewPic.document.
+  
+  var src = previewPic.getAttribute( "src" );
+  var alt = previewPic.getAttribute( "alt" );
+  document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+  document.getElementById('image').innerHTML = alt;
 }
  
-function unDo(){
-
-      /* In this function you should 
-     1) Update the url for the background image of the div with the id = "image" 
-     back to the orginal-image.  You can use the css code to see what that original URL was
-     
-     2) Change the text  of the div with the id = "image" 
-     back to the original text.  You can use the html code to see what that original text was
-     */
-     
+function unDo(X){
+  X = document.getElementById('image');
+  X.style.backgroundImage = "url('')";
+  document.getElementById('image').innerHTML = "Hover over an image below to display here";
+   
 }
